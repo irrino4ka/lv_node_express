28bda6dcc874fe46a8ddaf56b5907e6c9a1cad76
irrino4ka <ir.rino4ka@gmail.com>
2017-09-24 15:42:40 +0300

    add ejs view

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.send(`
-		<link rel="stylesheet" type="text/css" href="/style.css">
-		<h1>Eneida with routes.</h1>
-			<h2>Welcome!</h2>
-		`)
+	res.render('index');
 }) ;
 
 module.exports = router;
