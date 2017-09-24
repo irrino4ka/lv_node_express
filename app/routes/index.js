9a892989c7e57e11a3a74fd8d27876910081c88a
irrino4ka <ir.rino4ka@gmail.com>
2017-09-24 15:29:45 +0300

    add public folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.send(`<h1>Eneida with routes.</h1>
+	res.send(`
+		<link rel="stylesheet" type="text/css" href="/style.css">
+		<h1>Eneida with routes.</h1>
 			<h2>Welcome!</h2>
 		`)
 }) ;
