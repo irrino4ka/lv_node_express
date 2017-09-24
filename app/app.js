9e0fc463f08b5bd5a4c4a45d6b32b32e9e97fbb9
irrino4ka <ir.rino4ka@gmail.com>
2017-09-24 14:53:36 +0300

    Eneida with routes

--- app/app.js
+++ app/app.js
@@ -5,6 +5,13 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res){
+	res.send(`<h1>Eneida with routes.</h1>
+			<h2>Welcome!</h2>
+		`)
+}) ;
+
+
+app.get('/eneida', function(req, res){
 	var info = '';
 	dataFile.eneida.forEach(function(item){
 		info += `
@@ -16,6 +23,14 @@ app.get('/', function(req, res){
 	})
 	res.send(`<h1>Енеїда</h1>
 			${info}
+		`)
+}) ;
+
+app.get('/eneida/:eneidaid', function(req, res){
+	var eneidaRow = dataFile.eneida[req.params.eneidaid];
+	res.send(`
+		<h1>${eneidaRow.row}</h1>
+		<p>	${eneidaRow.text}</p>
 		`)
 }) ;
 
