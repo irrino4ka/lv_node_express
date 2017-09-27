149db99843f45cc3387ba702411a4df2710e4066
irrino4ka <ir.rino4ka@gmail.com>
2017-09-27 12:27:08 +0300

    add crud

--- app/app.js
+++ app/app.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var app = express();
 var dataFile = require('./data/data.json');
+var dataFileForTest = require('./data/myjsonfile.json');
+//const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
+app.set('appDataTest', dataFileForTest);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.locals.siteTitle = "Енеїда";
 //app.locals.allEneida = dataFile.eneida;
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/eneida'));
 app.use(require('./routes/api'));
+//app.use(express.methodOverride()); // поддержка put и delete
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
--- app/routes/eneida.js
+++ app/routes/eneida.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/eneida', function(req, res){
 	var data = req.app.get('appData');
@@ -30,6 +31,55 @@ router.get('/eneida/:eneidaid', function(req, res){
 	});
 }) ;
 
+router.post('/eneida', function(req, res){
+	var data = req.app.get('appDataTest');
+
+	var newRecord = {
+		row : req.body.row,
+		text : req.body.text
+	} 
+	data.eneida.push(newRecord)
+
+	fs.writeFile('./app/data/myjsonfile.json', JSON.stringify(data.eneida), 'utf8');
+});
+
+router.put('/eneida/:eneidaid', function(req, res){
+	var data = req.app.get('appDataTest');
+
+	var newRecord = {
+		row : req.body.row,
+		text : req.body.text
+	} 
+	var myIndex = data.eneida.map(function(item) { return item.row; }).indexOf(req.params.eneidaid);
+	//data.eneida.splice(myIndex,1);
+	data.eneida.splice(myIndex, 1, newRecord);
+	//data.eneida.push(newRecord)
+	console.log(data.eneida);
+	//fs.writeFile('./app/data/myjsonfile.json', JSON.stringify(data.eneida), 'utf8');
+});
+
+router.delete('/eneida/:eneidaid', function(req, res){
+	var data = req.app.get('appDataTest');
+	//console.log(data.eneida);
+	var myIndex = data.eneida.map(function(item) { return item.row; }).indexOf(req.params.eneidaid);
+	 //console.log(req.params.eneidaid);
+	console.log(data.eneida.map(function(item) { return item.row; }));
+	 //console.log(myIndex);
+
+	if(myIndex === -1) {
+	res.statusCode = 404;
+	return res.send('Error 404: No quote found');
+	}
+
+	data.eneida.splice(myIndex,1);
+
+	console.log(data.eneida)
+	fs.writeFile('./app/data/myjsonfile.json', JSON.stringify(data), 'utf8', function(err){
+		if(err) throw err;
+		res.json(true);
+	});
+
+});
 
 
 
@@ -43,4 +93,9 @@ router.get('/eneida/:eneidaid', function(req, res){
 		<h1>${eneidaRow.row}</h1>
 		<p>	${eneidaRow.text}</p>
 		`)
-}) ;*/
+}) ;
+
+
+
+
+*/
